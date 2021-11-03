5cb8d319098e6350d5322e77ffde4252bdd8836f
Okoromi Victor <okoromivictorsunday@gmail.com>
2021-11-03 20:17:09 +0100

    This is a startup file to add those learning, and who quickly want to kickoff with react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to your first React App</h1>
+      <p>Have a fun coding xperience</p>
+    </div>
+  );
+}
+
+export default App;
